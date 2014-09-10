40034b241b1bd8e5f4ba3d980b282e66068fde4c
joseramonc <joserracamacho@gmail.com>
2014-09-10 00:58:49 -0500

    init merge of oh-my-zsh with .dotfiles

---
+++ oh-my-zsh.symlink/oh-my-zsh.sh
@@ -0,0 +1,16 @@
+# Figure out the SHORT hostname
+if [[ "$OSTYPE" = darwin* ]]; then
+  # OS X's $HOST changes with dhcp, etc. Use ComputerName if possible.
+  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST=${HOST/.*/}
+else
+  SHORT_HOST=${HOST/.*/}
+fi
+
+# Save the location of the current completion dump file.
+if [ -z "$ZSH_COMPDUMP" ]; then
+  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+fi
+
+# Load and run compinit
+autoload -U compinit
+compinit -i -d "${ZSH_COMPDUMP}"
