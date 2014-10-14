1ee374832b91736686f61f77c4a4b1e2ddd6ab2e
Arandi Lopez <arandilopez.93@gmail.com>
2014-10-13 19:11:15 -0500

    added android path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.hub/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:/opt/android-sdk-linux/tools:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.hub/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:/opt/android-sdk-linux/platform-tools:/opt/android-sdk-linux/tools:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export ANDROID_HOME="/opt/android-sdk-linux"
