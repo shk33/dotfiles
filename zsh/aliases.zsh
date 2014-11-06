9b009e5bf067c4ef7ab859bbd533ff548a706aae
Arandi Lopez <arandilopez.93@gmail.com>
2014-11-05 20:01:02 -0600

    change some alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,8 @@
-alias reloadz='. ~/.zshrc'
+#alias reloadz='. ~/.zshrc'
 alias stt='subl .'
-alias st='subl '
-alias dotos='subl $ZSH'
+alias st='subl'
+alias dotos='atom $ZSH'
 alias at='atom'
 alias bootstrap='$ZSH/script/bootstrap'
 alias dotfiles='cd $ZSH'
-alias zeves='/opt/Z/Z-Eves/system/z-eves-gui.sh &' #ZEVES access... 
+alias zeves='/opt/Z/Z-Eves/system/z-eves-gui.sh &' #ZEVES access...
