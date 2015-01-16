462d3072dc9abf05658f72f7e22d4a063fb58887
Arandi Lopez <arandilopez.93@gmail.com>
2015-01-16 00:44:46 -0600

    changes for me

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,13 +1,12 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-hub_path=$(which hub)
+# hub_path=$(which hub)
 if (( $+commands[hub] ))
 then
-  alias git=$hub_path
+  alias git='hub'
 fi
 
 # The rest of my fun git aliases
-# alias git='hub'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -22,7 +21,7 @@ alias gcb='git copy-branch-name'
 alias gb='git branch -v'
 alias grmt='git remote -v'
 alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-#alias gst='git status'
+#alias gst='git status'  # just if you want to see all git comments
 alias glom='git pull origin master'
 alias gpom='git push origin master'
 alias g='git'
