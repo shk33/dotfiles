622cf65faa7aa8ae0c976a631b9c15154ddb8a1e
Arandi Lopez <arandilopez.93@gmail.com>
2015-02-05 00:02:21 -0600

    config gvm, golang.. etc

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,11 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.composer/vendor/bin:$HOME/.hub/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:/usr/local/heroku/bin:/opt/android-sdk-linux/platform-tools:/opt/android-sdk-linux/tools:/opt/genymotion:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export ANDROID_HOME="/opt/android-sdk-linux"
 export MOZILLA_FIVE_HOME="/usr/lib/mozilla"
+export GOROOT="/usr/local/go"
 # ssh
 export SSH_KEY_PATH="~/.ssh/dsa_id"
+
+
+# PATH
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.composer/vendor/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:/usr/local/heroku/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:/opt/genymotion:$GOROOT/bin:$PATH"
