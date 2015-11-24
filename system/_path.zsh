6d26fd52357852f6c22622af7f4bfc416f368836
Arandi Lopez <arandilopez.93@gmail.com>
2015-11-23 21:54:24 -0600

    android sdk and nodejs paths

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,6 @@
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export ANDROID_HOME="/opt/android-sdk-linux"
+export ANDROID_HOME="$HOME/.Android/Sdk"
 export MOZILLA_FIVE_HOME="/usr/lib/mozilla"
 export GOROOT="/usr/local/go"
 # ssh
@@ -9,4 +9,4 @@ export SSH_KEY_PATH="~/.ssh/dsa_id"
 # PATH
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.composer/vendor/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:/usr/local/heroku/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:/opt/genymotion:$GOROOT/bin:$PATH"
 export NVM_SYMLINK_CURRENT=true
-export NODE_PATH=$NODE_PATH:$HOME/.nvm/versions/node/v4.0.0/lib/node_modules
+export NODE_PATH=$NODE_PATH:$HOME/.nvm/versions/node/v5.0.0/lib/node_modules
