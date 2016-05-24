55007a334f3d6c14c5fa3a88fb438568dfb88eba
Arandi Lopez <arandilopez.93@gmail.com>
2016-05-24 10:22:50 -0500

    new ubuntu configs

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,11 +2,11 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export ANDROID_HOME="$HOME/.Android/Sdk"
 export MOZILLA_FIVE_HOME="/usr/lib/mozilla"
-export GOROOT="/usr/local/go"
+# export GOROOT="/usr/local/go"
 # ssh
 export SSH_KEY_PATH="~/.ssh/dsa_id"
 
 # PATH
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.composer/vendor/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:/usr/local/heroku/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:/opt/genymotion:$GOROOT/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.composer/vendor/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 export NVM_SYMLINK_CURRENT=true
-export NODE_PATH=$NODE_PATH:$HOME/.nvm/versions/node/v5.0.0/lib/node_modules
+export NODE_PATH=$NODE_PATH:$HOME/.nvm/versions/node/v6.2.0/lib/node_modules
