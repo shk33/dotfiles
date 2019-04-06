11372fc914941193aaf3662effe2ad1f959e54db
Eduardo Coronel <m.coronel.seg@gmail.com>
2019-04-06 12:18:29 -0600

    Settings

--- system/_path.zsh
+++ system/_path.zsh
@@ -7,10 +7,11 @@ export GOPATH="/usr/local/go/bin"
 export HUB="$HOME/hub-linux-amd64-2.2.9/bin"
 export ANDROID_STUDIO="/usr/local/android-studio/bin"
 export JAVA_HOME="/usr/lib/jvm/java-8-oracle/jre"
+export NODE_MODULES_PATH="./node_modules/.bin:$PATH"
 # ssh
 export SSH_KEY_PATH="~/.ssh/dsa_id"
 # PATH
-export FLUTER_PATH=$PATH:/home/miguel/flutter-sdk/flutter/bin
+export FLUTER_PATH=$PATH:/home/eduardo/flutter-sdk/flutter/bin
 export PATH="$PYENV_PATH:$JAVA_HOME:$ANDROID_STUDIO:$FLUTER_PATH:$HUB:$GOROOT:$GOPATH:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.config/composer/vendor/bin:$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 export NVM_SYMLINK_CURRENT=true
 export NODE_PATH=$NODE_PATH:$HOME/.nvm/versions/node/v6.11.0/lib/node_modules
